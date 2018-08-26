fc4faf5dc93ce146b6b7598861b9a6bef3c8ccc5
carrend <carrensignin@gmail.com>
2018-08-26 15:08:27 +0800

    add the source code

---
+++ exp_prepare.py
@@ -0,0 +1,101 @@
+import os, sys
+import csv
+import numpy as np
+from random import shuffle
+
+def walkDirWithExensions(image_folder, extensions):
+    image_list = []
+    for (dirpath, dirnames, filenames) in os.walk(image_folder):
+		for filename in filenames:
+			result_file_path = os.path.join(dirpath, filename)
+			if os.path.isfile(result_file_path) and (any(filename.endswith(item) for item in extensions )):
+				image_list.append(result_file_path)
+    return image_list
+
+def collectUserRating(csv_file):
+	# collect user rating of webpages from csv file
+	rating_list = {}
+	with open(csv_file, 'rb') as csvfile:
+		reader = csv.reader(csvfile)
+		reader.next()
+		i = 0
+		for row in reader:
+			image_name = str(row[1].strip())
+			rating = row[79].strip()
+			if not image_name in rating_list:
+				rating_list[image_name] = [float(rating)]
+			else:
+				rating_list[image_name].append(float(rating))
+	return rating_list
+	
+def generateListAll():
+	# get webpage rating list, per image name as dict key, all user rating as values
+	rate_file = '..\\data\\ae_only_unambiguous_1000.csv'
+	rating_list = collectUserRating(rate_file)
+	
+	result_folder = '..\\results\\'
+	if not os.path.exists(result_folder):
+		os.mkdir(result_folder)
+
+	# get webpage image list
+	img_folder = '..\\data\\'
+	extensions = ['png']
+	image_list = walkDirWithExensions(img_folder, extensions)
+	rated_image_list = []
+	for image in image_list:
+		csv_key = image.split('\\')[-2] + '_' + image.split('\\')[-1].split('.')[0]
+		if csv_key in rating_list:
+			rated_image_list.append(image)
+
+	shuffle(rated_image_list) # to random shuffle the whole dataset
+	train_list = rated_image_list[0:300]
+	valid_list = rated_image_list[300:338]
+	test_list = rated_image_list[338:398]
+
+	with open(os.path.join(result_folder, 'train_list.txt'), 'w') as f:
+		for image in train_list:
+			csv_key = image.split('\\')[-2] + '_' + image.split('\\')[-1].split('.')[0]
+			if csv_key in rating_list:
+				for rating in rating_list[csv_key]:
+					dummy = rating / 0.5 # to deal with *.5 rating cases, since caffe requires integer labels
+					if dummy%2 == 0:
+						f.write(image.split('\\')[-2] + '\\' + image.split('\\')[-1])
+						f.write(' ' + str(int(rating)))
+						f.write('\n')
+					else:
+						rng = np.random.RandomState(12345)
+						ratio = int(np.random.randint(2, size=1))
+						f.write(image.split('\\')[-2] + '\\' + image.split('\\')[-1])
+						f.write(' ' + str(int(rating)+1*ratio))
+						f.write('\n')
+					
+	with open(os.path.join(result_folder, 'valid_list.txt'), 'w') as f:
+		for image in valid_list:
+			csv_key = image.split('\\')[-2] + '_' + image.split('\\')[-1].split('.')[0]
+			if csv_key in rating_list:
+				f.write(image.split('\\')[-2] + '\\' + image.split('\\')[-1])
+				f.write(' ' + str(int(np.round(np.mean(rating_list[csv_key])))))
+				f.write('\n')
+	
+	with open(os.path.join(result_folder, 'test_list_eva.txt'), 'w') as f:
+		for image in test_list:
+			csv_key = image.split('\\')[-2] + '_' + image.split('\\')[-1].split('.')[0]
+			if csv_key in rating_list:
+				f.write(image.split('\\')[-2] + '\\' + image.split('\\')[-1])
+				f.write(' ' + str(np.round(np.mean(rating_list[csv_key]),2)))
+				f.write('\n')
+				
+	with open(os.path.join(result_folder, 'test_list.txt'), 'w') as f:
+		for image in test_list:
+			csv_key = image.split('\\')[-2] + '_' + image.split('\\')[-1].split('.')[0]
+			if csv_key in rating_list:
+				f.write(image.split('\\')[-2] + '\\' + image.split('\\')[-1])
+				f.write(' ' + str(int(np.round(np.mean(rating_list[csv_key])))))
+				f.write('\n')
+
+if __name__ == "__main__":
+	try:
+		# To random generate training, validation and test dataset
+		generateListAll()
+	except KeyboardInterrupt:
+		sys.exit(0)
---
+++ test_eva.py
@@ -0,0 +1,39 @@
+import sys
+from PIL import Image
+import caffe
+
+import itertools
+from scipy.stats import pearsonr
+
+def evaPearsonr(gt, pred):
+	return pearsonr(gt, pred)
+
+def getGT(gt_file):
+	gt = []
+	images = []
+	for line in open(gt_file, 'r'):
+		fle, rating = line.strip().split()
+		gt.append(float(rating))
+		images.append(fle)
+	return gt, images
+
+def infe():
+	net = caffe.Net('webthetics.prototxt','..\\experiments\\models\\soft_transfer\\flickr_iter_3700.caffemodel', caffe.TEST)
+	net.forward()
+	rating = net.blobs['fc8'].data
+	loss = net.blobs['loss'].data
+
+	gt_file = 'test_list_eva.txt'
+	gt, images = getGT(gt_file)
+	pred = list(itertools.chain.from_iterable(rating))
+	eva = evaPearsonr(gt, pred)
+
+	print eva
+	print 'Pearsonr: {}, p-value: {}, loss: {}'.format(eva[0], eva[1], loss)
+		
+
+if __name__ == '__main__':
+	try:
+		infe()
+	except KeyboardInterrupt:
+		sys.exit(0)
